169b5b19e9670c3585c562082d99148efdf28c52
Sergi Castillo Malpesa <sergi.castillo@scmspain.com>
2017-03-25 17:09:45 +0100

    No avengers

--- app/src/androidTest/java/com/karumi/screenshot/MainActivityTest.java
+++ app/src/androidTest/java/com/karumi/screenshot/MainActivityTest.java
@@ -77,6 +77,14 @@ public class MainActivityTest extends ScreenshotTest {
     compareScreenshot(activity);
   }
 
+  @Test public void showsNoAvengersSuperHeros() throws Exception {
+    givenThereAreSomeSuperHeroes(10, false);
+
+    Activity activity = startActivity();
+
+    compareScreenshot(activity);
+  }
+
   private List<SuperHero> givenThereAreSomeSuperHeroes(int numberOfSuperHeroes, boolean avengers) {
     List<SuperHero> superHeroes = new LinkedList<>();
     for (int i = 0; i < numberOfSuperHeroes; i++) {
