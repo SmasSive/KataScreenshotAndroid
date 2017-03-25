f7573c7f5c5c7fcf2ebd304d037465e83cbff2d0
Sergi Castillo Malpesa <sergi.castillo@scmspain.com>
2017-03-25 17:27:27 +0100

    Check a superhero with a long name

--- app/src/androidTest/java/com/karumi/screenshot/SuperHeroViewHolderTest.java
+++ app/src/androidTest/java/com/karumi/screenshot/SuperHeroViewHolderTest.java
@@ -47,6 +47,15 @@ public class SuperHeroViewHolderTest extends ScreenshotTest {
     compareScreenshot(holder, R.dimen.super_hero_row_height);
   }
 
+  @Test public void showsSuperHeroWithALongName() throws Exception {
+    SuperHero superHero = givenASuperHeroWithALongDescription();
+    SuperHeroViewHolder holder = givenASuperHeroViewHolder();
+
+    holder.render(superHero);
+
+    compareScreenshot(holder, R.dimen.super_hero_row_height);
+  }
+
   private SuperHeroViewHolder givenASuperHeroViewHolder() {
     Context context = getInstrumentation().getTargetContext();
     LayoutInflater inflater = LayoutInflater.from(context);
