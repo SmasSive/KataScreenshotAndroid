28eb94c15f4e6e5330ba8c41bfb816d9870efcb0
Sergi Castillo Malpesa <sergi.castillo@scmspain.com>
2017-03-25 17:41:24 +0100

    Test SuperHeroDetail - Any super hero

---
+++ app/src/androidTest/java/com/karumi/screenshot/SuperHeroDetailActivityTest.java
@@ -0,0 +1,60 @@
+package com.karumi.screenshot;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.rule.ActivityTestRule;
+import com.karumi.screenshot.di.MainComponent;
+import com.karumi.screenshot.di.MainModule;
+import com.karumi.screenshot.model.SuperHero;
+import com.karumi.screenshot.model.SuperHeroesRepository;
+import com.karumi.screenshot.ui.view.SuperHeroDetailActivity;
+import it.cosenonjaviste.daggermock.DaggerMockRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.mockito.Mock;
+
+import static org.mockito.Mockito.when;
+
+public class SuperHeroDetailActivityTest extends ScreenshotTest {
+
+  @Rule public DaggerMockRule<MainComponent> daggerRule =
+      new DaggerMockRule<>(MainComponent.class, new MainModule()).set(
+          new DaggerMockRule.ComponentSetter<MainComponent>() {
+            @Override public void setComponent(MainComponent component) {
+              SuperHeroesApplication app =
+                  (SuperHeroesApplication) InstrumentationRegistry.getInstrumentation()
+                      .getTargetContext()
+                      .getApplicationContext();
+              app.setComponent(component);
+            }
+          });
+
+  @Rule public ActivityTestRule<SuperHeroDetailActivity> activityRule =
+      new ActivityTestRule<>(SuperHeroDetailActivity.class, true, false);
+
+  @Mock SuperHeroesRepository repository;
+
+  @Test public void showsAnySuperHero() throws Exception {
+    SuperHero superHero = givenASuperHero();
+
+    Activity activity = startActivity(superHero);
+
+    compareScreenshot(activity);
+  }
+
+  private SuperHero givenASuperHero() {
+    String superHeroName = "Custom Hero";
+    SuperHero superHero = new SuperHero(superHeroName,
+        "http://www.myfreewallpapers.net/comics/wallpapers/captain-america-lives.jpg", false,
+        "Custom Description");
+    when(repository.getByName(superHeroName)).thenReturn(superHero);
+    return superHero;
+  }
+
+  private SuperHeroDetailActivity startActivity(SuperHero superHero) {
+    Intent intent = new Intent();
+    intent.putExtra("super_hero_name_key", superHero.getName());
+    return activityRule.launchActivity(intent);
+  }
+}
